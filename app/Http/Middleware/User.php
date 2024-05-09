8809d2beadce64553dc0ba2238ed81d28bc43fd3
websolutiondev2 <awebsolutionsp2@gmail.com>
2024-05-09 15:56:45 +0530

    Adding Middleware

---
+++ app/Http/Middleware/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class User
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $country =[
+            'India','USA','Canada','Russia'
+        ];
+
+        if(!isset($request->country) && !in_array($request->country,$country))
+        {
+            return redirect('notfound');
+        }
+
+        return $next($request);
+    }
+}
